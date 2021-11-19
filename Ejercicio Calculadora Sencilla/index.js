e987c712b83d6984a73caad16d5a2ec35624aaa9
Esteban Cortina <estebannsebastian@gmail.com>
2021-11-19 12:38:03 -0600

    Trabajo 19/11/2021 'funciones'

---
+++ Ejercicio Calculadora Sencilla/index.js
@@ -0,0 +1,52 @@
+function  suma() {
+   operacion = document.getElementById('operacion') .innerHTML= '+';
+}
+function  resta() {
+   operacion = document.getElementById('operacion') .innerHTML= '-';
+}
+function  multi() {
+   operacion = document.getElementById('operacion') .innerHTML= 'x';
+}
+function  div() {
+   operacion = document.getElementById('operacion') .innerHTML= '/';
+}
+
+function ac() {
+  // document.getElementById("formulario").reset();
+  n1.value = "";
+  n2.value = "";
+  }
+
+function igual() {
+
+  let a,b,r;
+  switch (operacion) {
+    
+    case '+':
+       a = parseInt(document.getElementById('n1').value);
+      b = parseInt(document.getElementById('n2').value);
+       r = a+b;
+      document.getElementById('r1').innerHTML = r.toString();
+      break;
+    case '-':
+        a = parseInt(document.getElementById('n1').value);
+        b = parseInt(document.getElementById('n2').value);
+        r = a-b;
+      document.getElementById('r2').innerHTML = r.toString();
+      break;
+    case 'x':
+       a = parseInt(document.getElementById('n1').value);
+       b = parseInt(document.getElementById('n2').value);
+       r = a*b;
+      document.getElementById('r3').innerHTML = r.toString();
+      break;
+    case '/':
+       a = parseInt(document.getElementById('n1').value);
+       b = parseInt(document.getElementById('n2').value);
+       r = a/b;
+      document.getElementById('r4').innerHTML = r.toString();
+      break;
+    
+  }
+  
+}
