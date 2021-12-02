cc9ef6171f421d372f7da2edd9c103bf8c666b80
Esteban Cortina <estebannsebastian@gmail.com>
2021-12-01 19:17:34 -0600

    01/12/2021

---
+++ extra.js
@@ -0,0 +1,11 @@
+function mostrar() {
+  if(document.getElementById('r1').checked){
+    alert('Esta seleccionado el primero');
+  }else if(document.getElementById('r2').checked){
+    alert('Esta seleeccionado el segundo');
+  }else if(document.getElementById('r3').checked){
+    alert('Esta seleccionado el tercero ')
+  }else{
+    alert('ninguno esta seleccionado');
+  }
+}
